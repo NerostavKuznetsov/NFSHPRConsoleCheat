7589a9ae547e937f2958bb2de353181e07cd9d3e
cjzkvbczxujhcvzxucgz <cjzkvbczxujhcvzxucgz@DESKTOP-3FUIS2S>
2025-09-29 06:43:50 -0300

    UPDATE RACER AND BOOST

--- RacerHacks/RacerWeaponsHack.cpp
+++ RacerHacks/RacerWeaponsHack.cpp
@@ -1,16 +1,23 @@
 ﻿#include <Includes.h>
 
-void FunctionRacerWeaponsHack(HANDLE hProcess)
+static void Banner() 
 {
-    system("cls");
-    COLOR_GREEN()
-    std::wcout << R"(
+std::wcout << R"(
 ________                              ___       __                                            ______  __            ______  
 ___  __ \_____ ___________________    __ |     / /__________ _____________________________    ___  / / /_____ _________  /__
 __  /_/ /  __ `/  ___/  _ \_  ___/    __ | /| / /_  _ \  __ `/__  __ \  __ \_  __ \_  ___/    __  /_/ /_  __ `/  ___/_  //_/
 _  _, _// /_/ // /__ /  __/  /        __ |/ |/ / /  __/ /_/ /__  /_/ / /_/ /  / / /(__  )     _  __  / / /_/ // /__ _  ,<   
 /_/ |_| \__,_/ \___/ \___//_/         ____/|__/  \___/\__,_/ _  .___/\____//_/ /_//____/      /_/ /_/  \__,_/ \___/ /_/|_|  
                                                              /_/                                                               )";
+}
+
+void FunctionRacerWeaponsHack(HANDLE hProcess)
+{
+	system("cls");
+    CONSOLE_CURSOR_INFO Cursor = { 1 };
+    SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &Cursor);
+    Banner();
+
     uintptr_t RacerWeaponsHack = modBaseAddr + 0x12791C0;
     uintptr_t FirstPointer = 0;
     uint32_t CurrentValue[4];
@@ -37,12 +44,12 @@ _  _, _// /_/ // /__ /  __/  /        __ |/ |/ / /  __/ /_/ /__  /_/ / /_/ /  / 
         }
     }
 
+    // ➡️ Status = true  -> todos os valores são 99
+    // ➡️ Status = false -> valores originais
     bool Status = (CurrentValue[0] == MaxSkillsValue && CurrentValue[1] == MaxSkillsValue && CurrentValue[2] == MaxSkillsValue && CurrentValue[3] == MaxSkillsValue);
-    // status = true  -> todos os valores são 99
-    // status = false -> valores originais
-
+    
     COLOR_RED()
-    std::wcout << L"\n\n[!] Current values:";
+    std::wcout << L"\n[!] Current values:";
     std::wcout << L"\n[!] SPIKES ----------------> " << CurrentValue[0];
     std::wcout << L"\n[!] TURBO -----------------> " << CurrentValue[1];
     std::wcout << L"\n[!] EMP -------------------> " << CurrentValue[2];
@@ -60,14 +67,14 @@ _  _, _// /_/ // /__ /  __/  /        __ |/ |/ / /  __/ /_/ /__  /_/ / /_/ /  / 
                 {
                     uintptr_t FinalAddress = FirstPointer + SkillsOffsets[i];
 
-					if (OldSkillsValue[i] == 0) // Se for a primeira vez que ativamos, salvamos o valor atual
+					if (OldSkillsValue[i] == 0) // ➡️ Se for a primeira vez que ativamos, salvamos o valor atual
                     {
                         ReadProcessMemory(hProcess, (LPCVOID)FinalAddress, &OldSkillsValue[i], sizeof(OldSkillsValue[i]), nullptr);
                     }
                     WriteProcessMemory(hProcess, (LPVOID)FinalAddress, &MaxSkillsValue, sizeof(MaxSkillsValue), nullptr);
                 }
                 COLOR_GREEN()
-                    std::wcout << L"\n[*] Racer Weapons -> ON (99 SET)";
+                std::wcout << L"\n[*] Racer Weapons -> ON (99 SET)";
             }
             else
             {
@@ -77,7 +84,7 @@ _  _, _// /_/ // /__ /  __/  /        __ |/ |/ / /  __/ /_/ /__  /_/ / /_/ /  / 
                     WriteProcessMemory(hProcess, (LPVOID)FinalAddress, &OldSkillsValue[i], sizeof(OldSkillsValue[i]), nullptr);
                 }
                 COLOR_GREEN()
-                    std::wcout << L"[*] Racer Weapons -> OFF (OLD VALUES SET)\n";
+                std::wcout << L"\n[*] Racer Weapons -> OFF (OLD VALUES SET)";
             }
 
             Status = !Status;
@@ -88,11 +95,19 @@ _  _, _// /_/ // /__ /  __/  /        __ |/ |/ / /  __/ /_/ /__  /_/ / /_/ /  / 
                 ReadProcessMemory(hProcess, (LPCVOID)FinalAddress, &CurrentValue[i], sizeof(CurrentValue[i]), nullptr);
             }
 
-            //COLOR_RED()
-            //std::wcout << L"\n[!] SPIKES ----------------> " << CurrentValue[0];
-            //std::wcout << L"\n[!] TURBO -----------------> " << CurrentValue[1];
-            //std::wcout << L"\n[!] EMP -------------------> " << CurrentValue[2];
-            //std::wcout << L"\n[!] JAMMER ----------------> " << CurrentValue[3] << L"\n";
+            Sleep(1666);
+			system("cls");
+
+			COLOR_GREEN()
+            Banner();
+            COLOR_RED()
+            std::wcout << L"\n[!] Current values:";
+            std::wcout << L"\n[!] SPIKES ----------------> " << CurrentValue[0];
+            std::wcout << L"\n[!] TURBO -----------------> " << CurrentValue[1];
+            std::wcout << L"\n[!] EMP -------------------> " << CurrentValue[2];
+            std::wcout << L"\n[!] JAMMER ----------------> " << CurrentValue[3];
+            std::wcout << L"\n\n[!] Racer Weapons are " << (Status ? L"ON (MAX VALUES)\n" : L"OFF (OLD VALUES)\n");
+            std::wcout << L"[!] Press INSERT to toggle Racer Weapons or ESC to return to the main menu...\n";
         }
 
         if (GetAsyncKeyState(VK_ESCAPE) & 1)
